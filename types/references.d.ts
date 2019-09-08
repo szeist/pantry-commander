e3c29e30123b0411c8812741d1c518fddecc8cfe
Istvan Szenasi <szeist@gmail.com>
2019-09-08 09:57:12 +0200

    rename spoonacular interface

--- types/references.d.ts
+++ types/references.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="../node_modules/tns-platform-declarations/android-26.d.ts" />
 
-/// <reference path="../app/lib/spoonacular/types.d.ts" />
+/// <reference path="../app/lib/spoonacular/interface.d.ts" />
